db82c7aaeeffb8aa8ec3c31f505d62d5036ca992
ravanaprabhu12 <149044532+ravanaprabhu12@users.noreply.github.com>
2024-02-22 07:46:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,17 +17,17 @@ global.autoreadgc = false;
 
 
 //
-global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+global.prefa = ["🦊"];                                    //Default prefix here. you can change if you want.
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["+919567813275", "+919567813275"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+919567813275", "+919567813275"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+919567813275"];
+global.OwnerName = "ABHIJITH";
+global.BotName = "MENTEL RAVANAN";
+global.packname = "𝅘𝅥𝅱𝄵𝄞𝑺𝑩⃢𝅋𝅍𝑷𝑺𝒀𝑪𝑶𝅋⃢𝅍𝑹𝑨𝑽𝑨𝑵𝑨𝑵𝄠𝆚𝅘𝅥𝅱";                             //Do not change.
+global.author = "ABHIJITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
@@ -41,12 +41,12 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "mathara, Sri Lankan";
+global.location = "kerala, Indian";
 global.reactmoji = "💞";
 global.themeemoji = "😂";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
 global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
-global.lolhuman = "kushan";
+global.lolhuman = "ABHIJITH";
 
 
 //
